b6e777edb92272dabbf2d9d78ec638f59c571f74
tanhm07 <tanhm07@hotmail.com>
2016-11-09 01:13:09 +0800

    v0.1
    user name character

---
+++ v0_1.c
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <time.h>
+
+using namespace std;
+
+int main () {
+
+char confirmKey = 'N';
+
+string str; 
+
+struct attributes {
+	string name;
+	int level;
+} player;
+
+player.level = 1;
+cout << "Welcome to RPG v0.1\n";
+while(confirmKey == 'N') {
+cout << "What would you like to name your character?\n";
+cout << "Name: ";
+getline(cin,player.name);
+cout << "Your character will be named " << player.name << ".\n";
+cout << "Is this correct? (Y/N): ";
+getline(cin, str);
+confirmKey = str[0];
+}
+cout << "Hi " << player.name << ".\n";
+cout << "You are currently level: " << player.level <<".\n";
+cout << "Welcome to the world.\nExciting adventures await you once I update this program more.\n";
+
+system("pause");
+return 0;
+}
