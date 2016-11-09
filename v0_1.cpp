b0a75311e89101c2b1bfa94be260d403c26ae86a
tanhm07 <tanhm07@hotmail.com>
2016-11-09 19:09:15 +0800

    goto function

--- v0_1.cpp
+++ v0_1.cpp
@@ -3,14 +3,22 @@
 #include <string>
 #include <time.h>
 #include <windows.h>
+#include <conio.h>
 
 using namespace std;
+
+HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE); // used for goto
+COORD CursorPosition; // used for goto
+
+void gotoXY(int,int); //function defined below
 
 int main () {
 
 char confirmKey = 'N';
 
 string str; 
+
+bool running = true;
 
 struct attributes {
 	string name;
@@ -19,9 +27,9 @@ struct attributes {
 
 player.level = 1;
 
-cout << "Welcome to RPG v0.1\n";
+cout << "Welcome to Custability Fighter v0.1\n";
 while(confirmKey != 'Y') {
-Sleep(2);
+Sleep(1000);
 cout << "What would you like to name your character?\n";
 cout << "Name: ";
 getline(cin,player.name);
@@ -31,9 +39,17 @@ getline(cin, str);
 confirmKey = str[0];
 }
 cout << "Hi " << player.name << ".\n";
+Sleep(1000);
 cout << "You are currently level: " << player.level <<".\n";
+Sleep(2000);
 cout << "Welcome to the world.\nExciting adventures await you once I update this program more.\n";
-
+Sleep(2000);
 system("pause");
 return 0;
+}
+
+void gotoXY(int x, int y) {
+	CursorPosition.X = x;
+	CursorPosition.Y = y;
+	SetConsoleCursorPosition(console,CursorPosition);
 }
